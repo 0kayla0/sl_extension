7519aa7ad44fef3a6f3a9c5c894f1f56e7488a11
Brandon Hua <branhua@gmail.com>
2019-06-25 12:20:06 -0600

    Add files via upload

--- popup.js
+++ popup.js
@@ -2,10 +2,12 @@ var xhr = new XMLHttpRequest();
 var url;
 var data = new Array(8);
 var data2 = new Array(8);
+var basic = true;
+var table
 //document.getElementById("result").innerHTML = xhr.responseText;
 // Update the relevant fields with the new data
 function setDOMInfo(info) {
-
+  
   url = info.URL;
   myFunction();
 }
@@ -23,7 +25,7 @@ function sendData() {
     "Feature-Policy"            : data[6],
     "Public-Key-Pins"           : data[7]
   };
-
+  
   xhr.open("POST", "http://129.82.174.202:1025/",0);
   xhr.setRequestHeader("Content-type", "application/json");
   xhr.send(JSON.stringify(variables));
@@ -35,8 +37,8 @@ function myFunction() {
   xhr.setRequestHeader('Access-Control-Expose-Headers', 'Content-Type, Location');
   xhr.send(200);
   
-
-  data[0] = xhr.getResponseHeader("strict-transport-security");
+  
+  data[0] = xhr.getResponseHeader("Strict-Transport-Security");
   data[1] = xhr.getResponseHeader("Content-Security-Policy");
   data[2] = xhr.getResponseHeader("x-frame-options");
   data[3] = xhr.getResponseHeader("x-xss-protection");
@@ -45,10 +47,11 @@ function myFunction() {
   data[6] = xhr.getResponseHeader("Feature-Policy");
   
   sendData();
-  for(var i = 0; i < data.length; i++){
-    data2[i] = ((data[i] === "undefined") || (data[i] === null)? "Missing" : "Present")
-  }
   
+  
+  for(var i = 0; i < data.length; i++){
+    data2[i] = (data[i] === null? "Missing" : "Present")
+  }
   document.getElementById("StrictTransportSecurity").innerHTML = data2[0];
   document.getElementById("ContentSecurityPolicy").innerHTML = data2[1];
   document.getElementById("XFrameOptions").innerHTML = data2[2];
@@ -56,17 +59,27 @@ function myFunction() {
   document.getElementById("XContentTypeOptions").innerHTML = data2[4];
   document.getElementById("ReferrerPolicy").innerHTML = data2[5];
   document.getElementById("FeaturePolicy").innerHTML = data2[6];
+
+  indicator(data);
 }
 
-function advanced(){
-  document.getElementById("StrictTransportSecurity").innerHTML = data[0];
-  document.getElementById("ContentSecurityPolicy").innerHTML = data[1];
-  document.getElementById("XFrameOptions").innerHTML = data[2];
-  document.getElementById("XXSSProtection").innerHTML = data[3];
-  document.getElementById("XContentTypeOptions").innerHTML = data[4];
-  document.getElementById("ReferrerPolicy").innerHTML = data[5];
-  document.getElementById("FeaturePolicy").innerHTML = data[6];
+//Add's a red/green indicator next to the corresponding security headers
+function indicator(data) {
+  var values = table.getElementsByTagName("td");
+  for(var i = 0; i < data.length; i++){
+    values[i*3].style.backgroundColor = (data[i] === null ? "red" : "green") ;
+  }
 }
+
+//Switches information from basic to advanced based on a boolean TODO: This only works once...
+function change(){
+  let offset = 2;
+  var x = table.getElementsByTagName("td");
+  for(var i = 0; i < data.length; i++){
+    x[(i*3) + offset].innerHTML = data[i];
+  }
+}
+
 window.addEventListener('DOMContentLoaded', function () {
   // ...query for the active tab...
   chrome.tabs.query({
@@ -84,5 +97,6 @@ window.addEventListener('DOMContentLoaded', function () {
 });
 //add an event listener for the button being pushed
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button').addEventListener('click', advanced);
+  table = document.getElementById("attributes")
+  document.getElementById("change").addEventListener('click', change);
 });
